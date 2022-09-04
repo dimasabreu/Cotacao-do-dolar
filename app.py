2fe8a247c7494ed1a45bc76e536fe072f55eb677
Dimas Abreu <dimasabreu@live.com>
2022-09-03 21:01:33 -0300

    Testando streamlit

---
+++ app.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+from download import download
+from forex_python.converter import CurrencyRates
+
+
+def main():
+    download()
+    # configurações da tabela
+    st.set_page_config(page_title="Lista de Moeda", 
+                    page_icon=":bar_chart:",
+                    layout="wide")
+    # criando a tabela 
+    df = pd.read_excel(
+        io='Countries list.xlsx',
+        engine='openpyxl',
+        sheet_name='Sheet1',
+        skiprows=1,
+        usecols='A:C'
+    )
+    listademoedas = []
+    # divindo em colunas
+    for item in df['Moedas']:
+        listademoedas.append(item)
+    listademoedas.append('TODOS')
+    listademoedas.append('TODAS')
+    
+    with st.form(key='searchMoeda', clear_on_submit=True):
+        search_Currencies = st.text_input("Qual moeda você esta procurando?").upper()
+        submit_button = st.form_submit_button(label='Procurar')
+
+        if submit_button:
+            if search_Currencies not in listademoedas:
+                st.info('A moeda digitada não existe')
+            elif search_Currencies == "TODOS" or search_Currencies == "TODAS":
+                st.info('Você procurou por : Todas as moedas')
+                results = df
+                st.write(results)
+            else:
+                col1, col2 = st.columns([3,2])
+                with col1:
+                    st.info('Os seguintes Países usam {} como moeda:'.format(search_Currencies))
+                    results = df.loc[df['Moedas'] == search_Currencies]
+                    st.write(results)
+                with col2:
+                    c = CurrencyRates()
+                    try:
+                        preco = c.get_rate(search_Currencies, 'BRL')
+                        st.info('Preço Atual: R$ {:,.2f}'.format(preco))
+                    except:
+                        st.info('Moeda fora do rastreio')
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ download.py
@@ -0,0 +1,124 @@
+import requests
+import pandas as pd
+from formating import treatment
+
+def download():
+    print('Your request is being processed...')
+    # first thing geting the right api
+    request = requests.get("https://restcountries.com/v2/all?fields=name,capital,area,currencies").json()
+
+    # Creating a list for each object i'm getting so i can work on it 
+    Name = []
+    Capital = []
+    areas = []
+    currencies = []
+
+    def format_float(value):
+        return f'{value:_.2f}'
+
+    # Cleaning country names
+    for pais in request:
+        name = pais['name']
+        Name.append(name)
+
+    # Cleaning capital names
+    for city in request:
+        cidade = city.get('capital')
+        if cidade == None:
+            Capital.append("-")
+        else: 
+            Capital.append(cidade)
+
+    # Cleaning areas
+    for tamanho in request:
+        area = tamanho.get('area')
+        if area == None:
+            areas.append("-")
+        else:
+            area = format_float(area)
+            area = area.replace(".", ",").replace("_", ".")
+            areas.append(area)
+
+    # Cleaning currencies 
+    for moedas in request:
+        moeda = moedas.get('currencies')
+        if moeda == None:
+            currencies.append('-')
+        else:
+            currencies.append(moeda)
+
+
+
+
+    # starting a df from the raw request
+    df = pd.DataFrame(request)
+    # putting the cleaner data in the df
+    df['Paises'] = Name
+    df['Capitais'] = Capital
+    df['Area'] = areas
+    df['Moedas'] = currencies
+
+    # taking of the old data
+    df.pop('independent')
+    df.pop('name')
+    df.pop('capital')
+    df.pop('area')
+    df.pop('currencies')
+
+    # organizing rows
+    nome_coluna = ['Paises', 'Capitais', "Area", 'Moedas']
+    df = df.reindex(columns=nome_coluna)
+
+    # starting the excel writer
+    writer = pd.ExcelWriter('Countries list.xlsx', engine='xlsxwriter')
+
+    # Convert the dataframe to an XlsxWriter Excel object.
+    df.to_excel(writer, sheet_name='List', index=False)
+
+    # Close the Pandas Excel writer and output the Excel file.
+    writer.save()
+
+    # Next cleaning phase, starting the excel reader
+    Paises = pd.read_excel('Countries list.xlsx')
+
+    # Creating a variable to split the currencies based on : to get what i need
+    variable_split = Paises['Moedas'].str.split(':')
+    Paises['Moedas'] = variable_split.str.get(1)
+    Paises['Currencies2'] = variable_split.str.get(4)
+
+    # Creating another variable to split it again
+    variable_split2 = Paises['Moedas'].str.split(',')
+    Paises['Moedas'] = variable_split2.str.get(0)
+    variable_splitando = Paises['Currencies2'].str.split(',')
+    Paises['Currencies2'] = variable_splitando.str.get(0)
+    Paises.to_excel('Countries list.xlsx', index=False)
+
+    # Cleaning the last column, by spliting it again
+    Coins = pd.read_excel('Countries list.xlsx')
+    variable_split3 = Paises['Moedas'].str.split("'")
+    Paises['Moedas'] = variable_split3.str.get(1)
+    variable_splitando2 = Paises['Currencies2'].str.split("'")
+    Paises['Currencies2'] = variable_splitando2.str.get(1)
+    Paises.to_excel('Countries list.xlsx', index=False)
+    df = pd.DataFrame(['-'], columns=list('D'), index=[10])
+
+    # Filling gaps
+    data = pd.read_excel('Countries list.xlsx')
+    data['Moedas'].fillna("-", inplace=True)
+
+    # using the second curriencie to adapt the places with 2 or more currencies
+    data['Currencies2'].fillna(" ", inplace=True)
+    df = data
+    df['Moedas'] = df['Moedas'].map(str) + "," + df['Currencies2']
+    m = df["Moedas"].str.endswith(', ')
+    df["Moedas"].loc[m] =  df["Moedas"].loc[m].str[:-2]
+    df.pop('Currencies2')
+    df.pop('Area')
+    df.to_excel('Countries list.xlsx', index=False)
+    # calling a function to treat the data
+    treatment()
+    print("Aquivo criado")
+
+
+if __name__ == '__main__':
+    download()
